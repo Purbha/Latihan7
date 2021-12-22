3e31977f8e3358747e6f65868aa303871de54a3a
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-12-22 21:25:00 +0700

    UPDATE 20211222 21:07

--- app/src/main/java/com/ims_hr/latihan7/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan7/MainActivity.java
@@ -1,10 +1,11 @@
 package com.ims_hr.latihan7;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
